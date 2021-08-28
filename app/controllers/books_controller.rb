f0c51a1fc6a13c82ecbd208e22679dd1ddf663ff
AlanSoto31 <soto.alan@hotmail.com>
2021-08-28 17:23:33 -0500

    Add book list to home page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,10 @@
 class BooksController < ApplicationController
-
+  def index
+    @books = Book.all
+  end
+  
   def new
     user_must_be_seller(new_book_path)
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
+  root 'books#index'
   get 'signup', to: 'users#new'
   get 'login', to: 'session#new'
   post 'login', to: 'session#create'
   get 'logout', to: 'session#destroy'
 
   resources :users, only:[:new, :create, :edit, :update]
-  resources :books, only:[:new]
+  resources :books, only:[:index, :new]
   resources :buy, only:[:new]
   
 end
