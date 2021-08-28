8032964b5d727971836104daf86b4c9577a1f928
AlanSoto31 <soto.alan@hotmail.com>
2021-08-28 16:49:13 -0500

    Generate Book model and relationship with User model

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+
   def new
     user_must_be_seller(new_book_path)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_27_154322) do
+ActiveRecord::Schema.define(version: 2021_08_28_213649) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "author"
+    t.decimal "price"
+    t.bigint "user_id", null: false
+    t.boolean "available", default: true
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -27,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_08_27_154322) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "books", "users"
 end
