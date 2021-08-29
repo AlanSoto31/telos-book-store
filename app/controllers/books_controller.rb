49ff47f06fd6abfe9e2f3410066147882f65d577
AlanSoto31 <soto.alan@hotmail.com>
2021-08-28 19:16:14 -0500

    Add search by title in books index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   def index
-    @books = Book.all
+    @books = Book.search(params[:search])
   end
   
   def new
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,8 @@
 class Book < ApplicationRecord
   belongs_to :user
+
+  def self.search(search)
+      Book.find_by(title: search) ? Book.where(title: search) : Book.all
+  end
+
 end
