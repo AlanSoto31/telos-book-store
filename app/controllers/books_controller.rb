505b01fdadfcabdc0e52c559740befac656f6adc
AlanSoto31 <soto.alan@hotmail.com>
2021-08-30 17:09:00 -0500

    Logged in user is able to edit or delete his own books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,34 @@ class BooksController < ApplicationController
     end
   end
 
+  def edit
+    @book = find_book
+  end
+
+  def update
+    @book = find_book
+      if @book.update(book_params)
+        redirect_to root_path, notice: "Book was successfully updated"
+      else
+        flash[:alert] = "Something went wrong"
+        render 'edit'
+      end
+  end
+  
+  def destroy
+    @book = find_book
+    if @book.destroy
+      redirect_to root_path, notice: 'Book was successfully deleted.'
+    else
+      flash[:alert] = 'Something went wrong'
+    end
+  end
+
   private
+
+  def find_book
+    Book.find(params[:id])
+  end
 
   def book_params
     params.require(:book).permit(:title, :description, :author, :price, :user_id, :available)
