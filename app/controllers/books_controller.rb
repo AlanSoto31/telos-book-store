00652445569736a6803e45cc85aa54b6f85d5ed5
AlanSoto31 <soto.alan@hotmail.com>
2021-08-30 15:53:46 -0500

    Add create method to Book controller and new book form to Book views

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,5 +5,23 @@ class BooksController < ApplicationController
 
   def new
     user_must_be_seller(new_book_path)
+    @book = Book.new
   end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to root_path, notice: "Book successfully created"
+    else
+      flash.now[:alert] = "Something went wrong"
+      render 'new'
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :description, :author, :price, :user_id, :available)
+  end
+  
 end
