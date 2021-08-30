15cb504d49ad6a0eee9333598bb6ffdd9b80989f
AlanSoto31 <soto.alan@hotmail.com>
2021-08-30 14:12:36 -0500

    Include app revenue to seller results view

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -3,11 +3,10 @@ class Sale < ApplicationRecord
   belongs_to :book
 
   def self.earnings(sales)
-    app_revenue = 1
     acc = 0
     sales.each do |book|
       acc += book.price
     end
-    acc - app_revenue
+    acc
   end
 end
