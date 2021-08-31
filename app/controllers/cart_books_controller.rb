f2b5353d57851dda7dfd4438ce828c923d4cba9a
AlanSoto31 <soto.alan@hotmail.com>
2021-08-31 15:14:16 -0500

    Set basic connection through ActionCable

--- app/controllers/cart_books_controller.rb
+++ app/controllers/cart_books_controller.rb
@@ -5,6 +5,7 @@ class CartBooksController < ApplicationController
     @current_cart = current_cart
     c_book = Book.find(params[:book_id])
     c_book.update(available: false)
+    ActionCable.server.broadcast "room_channel", "Hello"
     cart_book = CartBook.new(book_id: params[:book_id], cart_id: @current_cart.id)
     cart_book.save
     redirect_to root_path
