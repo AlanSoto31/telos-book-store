38c30afd6e9c70640775361e986b6e751c39a5e7
AlanSoto31 <soto.alan@hotmail.com>
2021-09-01 05:56:36 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,15 +12,15 @@ user3 = User.create(name: 'hana', email: 'hana@hana', address: 'acambaro', phone
 
 p "Created #{User.count} users"
 
-Book.create(title: 'Poema de Gilgamesh', description: 'description b1' author: 'Anónimo', price: 15.00, seller: user1.id)
-Book.create(title: 'Todo se desmorona', description: 'description b2' author: 'Chinua Achebe', price: 15.00, seller: user2.id)
-Book.create(title: 'Divina comedia', description: 'description b3' author: 'Dante Alighieri', price: 15.00, seller: user3.id)
-Book.create(title: 'Orgullo y prejuicio', description: 'description b4' author: 'Jane Austen', price: 15.00, seller: user1.id)
-Book.create(title: 'Decamerón', description: 'description b5' author: 'Giovanni Boccaccio', price: 15.00, seller: user2.id)
-Book.create(title: 'Ficciones', description: 'description b6' author: 'Jorge Luis Borges', price: 15.00, seller: user3.id)
-Book.create(title: 'Cumbres Borrascosas', description: 'description b7' author: 'Emily Brontë', price: 15.00, seller: user1.id)
-Book.create(title: 'Don Quijote de la Mancha', description: 'description b8' author: 'Miguel de Cervantes', price: 15.00, seller: user2.id)
-Book.create(title: 'Grandes Esperanzas', description: 'description b9' author: 'Charles Dickens', price: 15.00, seller: user3.id)
-Book.create(title: 'Crimen y castigo', description: 'description b10' author: 'Fiódor Dostoievski', price: 15.00, seller: user1.id)
+Book.create(title: 'Poema de Gilgamesh', description: 'description b1', author: 'Anónimo', price: 15.00, user_id: user1.id)
+Book.create(title: 'Todo se desmorona', description: 'description b2', author: 'Chinua Achebe', price: 15.00, user_id: user2.id)
+Book.create(title: 'Divina comedia', description: 'description b3', author: 'Dante Alighieri', price: 15.00, user_id: user3.id)
+Book.create(title: 'Orgullo y prejuicio', description: 'description b4', author: 'Jane Austen', price: 15.00, user_id: user1.id)
+Book.create(title: 'Decamerón', description: 'description b5', author: 'Giovanni Boccaccio', price: 15.00, user_id: user2.id)
+Book.create(title: 'Ficciones', description: 'description b6', author: 'Jorge Luis Borges', price: 15.00, user_id: user3.id)
+Book.create(title: 'Cumbres Borrascosas', description: 'description b7', author: 'Emily Brontë', price: 15.00, user_id: user1.id)
+Book.create(title: 'Don Quijote de la Mancha', description: 'description b8', author: 'Miguel de Cervantes', price: 15.00, user_id: user2.id)
+Book.create(title: 'Grandes Esperanzas', description: 'description b9', author: 'Charles Dickens', price: 15.00, user_id: user3.id)
+Book.create(title: 'Crimen y castigo', description: 'description b10', author: 'Fiódor Dostoievski', price: 15.00, user_id: user1.id)
 
 p "Created #{Book.count} books"
