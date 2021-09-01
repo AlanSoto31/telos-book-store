89df281b223ba0563c642478e6ce4faf700144fa
AlanSoto31 <soto.alan@hotmail.com>
2021-09-01 05:36:52 -0500

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,33 +10,17 @@ user1 = User.create(name: 'alan', email: 'alan@alan', address: 'acambaro', phone
 user2 = User.create(name: 'ana', email: 'ana@ana', address: 'acambaro', phone: '123456', password: '123456', seller: true)
 user3 = User.create(name: 'hana', email: 'hana@hana', address: 'acambaro', phone: '123456', password: '123456', seller: true)
 
-(1..3).each do
-    Book.create({
-        title: Faker::Book.title,
-        description: "Description",
-        author: Faker::Book.author,
-        price: 15.00,
-        user_id: user1.id,
-    })
-end
+p "Created #{User.count} users"
 
-(1..3).each do
-    Book.create({
-        title: Faker::Book.title,
-        description: "Description",
-        author: Faker::Book.author,
-        price: 15.00,
-        user_id: user2.id,
-    })
-end
+Book.create(title: 'Poema de Gilgamesh', description: 'description b1' author: 'Anónimo', price: 15.00, seller: user1.id)
+Book.create(title: 'Todo se desmorona', description: 'description b2' author: 'Chinua Achebe', price: 15.00, seller: user2.id)
+Book.create(title: 'Divina comedia', description: 'description b3' author: 'Dante Alighieri', price: 15.00, seller: user3.id)
+Book.create(title: 'Orgullo y prejuicio', description: 'description b4' author: 'Jane Austen', price: 15.00, seller: user1.id)
+Book.create(title: 'Decamerón', description: 'description b5' author: 'Giovanni Boccaccio', price: 15.00, seller: user2.id)
+Book.create(title: 'Ficciones', description: 'description b6' author: 'Jorge Luis Borges', price: 15.00, seller: user3.id)
+Book.create(title: 'Cumbres Borrascosas', description: 'description b7' author: 'Emily Brontë', price: 15.00, seller: user1.id)
+Book.create(title: 'Don Quijote de la Mancha', description: 'description b8' author: 'Miguel de Cervantes', price: 15.00, seller: user2.id)
+Book.create(title: 'Grandes Esperanzas', description: 'description b9' author: 'Charles Dickens', price: 15.00, seller: user3.id)
+Book.create(title: 'Crimen y castigo', description: 'description b10' author: 'Fiódor Dostoievski', price: 15.00, seller: user1.id)
 
-(1..4).each do
-    Book.create({
-        title: Faker::Book.title,
-        description: "Description",
-        author: Faker::Book.author,
-        price: 15.00,
-        user_id: user3.id,
-    })
-end
-
+p "Created #{Book.count} books"
